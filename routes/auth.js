35dabf82c2b10e5a454a3effa3c26bfe9008786b
am <risheek@gmail.com>
2025-06-23 13:13:19 -0700

    return role on login plus token

--- routes/auth.js
+++ routes/auth.js
@@ -41,7 +41,7 @@ router.post('/login', async (req, res) => {
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    res.json({ token });
+    res.json({ token, role: user.role });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
