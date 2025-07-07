66d221a07eb614651e29bff1e199f3e394fa016a
am <risheek@gmail.com>
2025-07-07 14:48:18 -0700

    fix typo

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
-
 const InvoiceSchema = new mongoose.Schema({
   invoiceNumber: { type: String, required: true, unique: true }, // You can generate this in the backend
   fromUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
